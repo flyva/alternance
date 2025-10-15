c013d6f2e10e6faa14202750c2bbe5ddbd3962aa
Quentin <quentinrault0@gmail.com>
2025-10-15 23:56:58 +0200

    MAJ HTML du site

--- php/billeterie.php
+++ php/billeterie.php
@@ -9,48 +9,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $phone_ticket = $_POST['ticket-form-phone'];
     $mail_ticket = $_POST['ticket-form-mail'];
 
-    // Nombre de billets récupéré depuis le formulaire
-    $nb_tickets = $_POST['ticket-form-number'] ?? 1;
 
     // On commence avec le CV de base
     $attachment_mail = ["../attachment_file_mail/CV_quentin_rault.pdf"];
 
-    // Si plus d'un billet, on ajoute CV2, CV3...
-    if ($nb_tickets >= 2) {
-        $attachment_mail[] = "../attachment_file_mail/CV2_quentin_rault.pdf";
-    }
-    if ($nb_tickets >= 3) {
-        $attachment_mail[] = "../attachment_file_mail/CV3_quentin_rault.pdf";
-    }
-
-    $type_ticket = $_POST['TicketForm'] ?? null;
-    $number_ticket = $_POST['ticket-form-number'];
     $message_forms = $_POST['ticket-form-message'];
 
     // Paramètre client
-    $body_mail_client = (function() use ($type_ticket) {
-        if ($type_ticket === 'Opt_1') {
-            return "
-<p><strong>Merci pour votre message&nbsp;!</strong></p>
-    <p>Je ne manquerais pas de vous répondre dans les plus bref délais.</p>
-    <p>En vous souhaitant une excellente journée/soirée,</p>
-    <p>Cordialement,</p>
-    <p>Quentin RAULT</p>
-    ";
-        } else {
-            return "
+    $body_mail_client ="
    <p><strong>Merci pour votre message&nbsp;!</strong></p>
        <p>Je ne manquerais pas de vous répondre dans les plus bref délais.</p>
        <p>En vous souhaitant une excellente journée/soirée,</p>
        <p>Cordialement,</p>
        <p>Quentin RAULT</p>
     ";
-        }
-    })();
+
 
 
     // Envoi du mail client
-    mail_send($mail_ticket, $name_ticket, $attachment_mail, "Vos billets", $body_mail_client);
+    mail_send($mail_ticket, $name_ticket, $attachment_mail, "Billeterie", $body_mail_client);
 
 
 
@@ -58,19 +35,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Paramètre hote
 
-    if ($type_ticket === 'Opt_1') {
-        $type_text = 'Un entretien';
-    } else {
-        $type_text = 'Une alternance';
-    }
-
     $body_mail_host = "
     <p>Nom: {$name_ticket}</p>
     <p>Téléphone: {$phone_ticket}</p>
     <p><strong>Mail:</strong> {$mail_ticket}</p>
     <br>
-    <p><strong>Type de billet:</strong> {$type_text}</p>
-    <p><strong>Nombre de billet:</strong> {$number_ticket}</p>
     <p><strong>Message:</strong> {$message_forms}</p>
     ";
 
